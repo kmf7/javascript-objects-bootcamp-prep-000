46a192feba10c000eb90c527e2aa9d309d3b6763
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-01-27 19:10:14 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist= {Jewel:'Hands'}
+function updatePlaylist(playlist,artistname, songtitle){
+  playlist[artistname]=songtitle
+  return playlist
+}
+function removeFromPlaylist(playlist,artistname){
+  delete.playlist.artistname
+  return playlist
+}
