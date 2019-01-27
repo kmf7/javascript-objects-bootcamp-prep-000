b832248c962ad3db161774f3c73f8b99392a9e10
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-01-27 19:12:02 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ function updatePlaylist(playlist,artistname, songtitle){
   playlist[artistname]=songtitle;
   return playlist;
 }
-function removeFromPlaylist(playlist,artistname){
-  delete playlist.artistname;
+function removeFromPlaylist(playlist,artistName){
+  delete playlist.artistName;
   return playlist;
 }
